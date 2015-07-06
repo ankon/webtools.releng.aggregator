5f67a4d2418879256a489022cd4d4d54ef75cbf8
Vaninder Rajput <vaninder@ca.ibm.com>
2015-07-06 13:45:32 -0400

    Change version from 3.7.0 to 3.8.0 for new WTP builds for Eclipse Neon

--- scripts/build-preparation.sh
+++ scripts/build-preparation.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#Replaces @buildversionid@ by 3.7.0, in the category.xml files.
-find ./ -name category.xml -type f -exec sed -i 's/@buildversionid@/3.7.0/g' {} \;
+#Replaces @buildversionid@ by 3.8.0, in the category.xml files.
+find ./ -name category.xml -type f -exec sed -i 's/@buildversionid@/3.8.0/g' {} \;
