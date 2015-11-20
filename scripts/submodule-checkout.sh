381c05ab622a61923e86719b51545af7a4fd7bd5
Carl Anderson <ccc@us.ibm.com>
2015-11-20 14:50:40 -0500

    Prep for Apache Commons Collections fix

--- scripts/submodule-checkout.sh
+++ scripts/submodule-checkout.sh
@@ -7,11 +7,10 @@
 # Usage: ./scripts/submodules-checkout.sh
 #
 # This script should be called from the root of the aggregator repo.
-
 # There are two list of repositories, the "normal" and the "patches" the
 # In the patches one, only those components that had requested to be included
 # in the patch will be used, saving time in the preparation of the environment
-IFS=$'\n' read -d '' -r -a repos < scripts/repositories.txt
+IFS=$'\n' read -d '' -r -a repos < scripts/patches-repositories.txt
 
 for i in "${repos[@]}"
 do
