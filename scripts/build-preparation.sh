1e68a77e72aedc14ca589ff8eb0b7a82e20eed58
Andreas Kohn <andreas.kohn@gmail.com>
2016-05-23 11:47:39 +0200

    Make build-preparation.sh work on MacOS X

--- scripts/build-preparation.sh
+++ scripts/build-preparation.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #Replaces @buildversionid@ by 3.8.0, in the category.xml files.
-find ./ -name category.xml -type f -exec sed -i 's/@buildversionid@/3.8.0/g' {} \;
+find ./ -name category.xml -type f -exec sed -i'' -e 's/@buildversionid@/3.8.0/g' {} \;
